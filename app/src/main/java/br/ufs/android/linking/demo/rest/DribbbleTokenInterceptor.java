af3d0820e3b0ffc95abf27e579a0aae7e59deb03
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2015-10-18 17:04:32 -0300

    Code cleaning up

--- app/src/main/java/br/ufs/android/linking/demo/rest/DribbbleTokenInterceptor.java
+++ app/src/main/java/br/ufs/android/linking/demo/rest/DribbbleTokenInterceptor.java
@@ -8,10 +8,11 @@ import retrofit.RequestInterceptor;
 
 public class DribbbleTokenInterceptor implements RequestInterceptor {
 
-    public static final String ACCESS_TOKEN = null;
+    public static final String ACCESS_TOKEN =
+            "bc1b249502dece98d6c157b3e0274e97b9f43a04d81b76c6ccbadfc7fff63362";
 
     @Override public void intercept(RequestFacade request) {
-        if (ACCESS_TOKEN == null)
+        if(ACCESS_TOKEN == null)
             throw new IllegalArgumentException
                     ("Use this demo with a valid Dribbble Access Token!!");
 
