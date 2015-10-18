5900e798f925f63e8626a276aada1b4e2611489f
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2015-10-18 17:00:26 -0300

    Removing Dribbble access token for open sourcing

--- app/src/main/java/br/ufs/android/linking/demo/rest/DribbbleTokenInterceptor.java
+++ app/src/main/java/br/ufs/android/linking/demo/rest/DribbbleTokenInterceptor.java
@@ -8,10 +8,13 @@ import retrofit.RequestInterceptor;
 
 public class DribbbleTokenInterceptor implements RequestInterceptor {
 
-    public static final String ACCESS_TOKEN =
-            "bc1b249502dece98d6c157b3e0274e97b9f43a04d81b76c6ccbadfc7fff63362";
+    public static final String ACCESS_TOKEN = null;
 
     @Override public void intercept(RequestFacade request) {
+        if (ACCESS_TOKEN == null)
+            throw new IllegalArgumentException
+                    ("Use this demo with a valid Dribbble Access Token!!");
+
         request.addHeader("Authorization", "Bearer " + ACCESS_TOKEN);
     }
 }
